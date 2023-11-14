10d3df756963829330fc221c8ce8777244413c72
Victor Venegas <victor.venegas.call@gmail.com>
2023-11-14 14:07:31 -0300

    peticion http con dio

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/helpers/get_yes_no_answer.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
+  final getYesNoAnswer = GetYesNoAnswer(); 
   final ScrollController chatScrollController = ScrollController( );
 
   List <Message> messageList = [
@@ -14,9 +16,22 @@ class ChatProvider extends ChangeNotifier {
     if ( text.isEmpty ) return;
     final newMessage = Message(text: text, fromWho: FromWho.me);
     messageList.add(newMessage);
+
+    if( text.endsWith('?')){
+      otherReplay();
+    }
     
     notifyListeners();
     moveScrolltoBottom();
+  }
+
+  Future<void> otherReplay() async{
+    final otherMessage = await getYesNoAnswer.getAnswer();
+
+    messageList.add(otherMessage);
+    notifyListeners();
+    moveScrolltoBottom();
+
   }
 
   Future<void> moveScrolltoBottom () async{
